7d7a0e4a860b1c6e453b35fe5b7c9149084c29e7
Leys <palabayleo20@gmail.com>
2023-06-24 21:06:18 +0800

    clean code

--- array name with vandal stats.cpp
+++ array name with vandal stats.cpp
@@ -4,16 +4,15 @@ using namespace std;
 int main() {
     
     /*
-Vandal Stats
-Create a stats program for the famous valorant gun. The program should determine the amount of bullet that the gun have. Ask the user for the current number of ammo, ask if how many shots are fired and ask if he wants to reload. Reloading the gun refreshes the number of bullets to 30 and firing the gun diminishes the number of ammo. Then diisplay the total number of bullets.
+    Vandal Stats
+    Create a stats program for the famous valorant gun. The program should determine the amount of bullet that the gun have. Ask the user for the current number of ammo, ask if how many shots are fired and ask if he wants to reload. Reloading the gun refreshes the number of bullets to 30 and firing the gun diminishes the number of ammo. Then diisplay the total number of bullets.
 
-Example
-Enter number of ammo: 30
-How many shots fired: 28
-Do you want to reload? 1 - yes 0 - no  
-You have 2 bullets left.
-    
-    
+    Example
+    Enter number of ammo: 30
+    How many shots fired: 28
+    Do you want to reload? 1 - yes 0 - no  
+    You have 2 bullets left.
+     
     */
    
    
