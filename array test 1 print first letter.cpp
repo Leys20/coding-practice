02f855cde27eec4b671cb2f9b26091fb5012cf24
Leys <palabayleo20@gmail.com>
2023-06-27 05:06:33 +0800

    solve array problem

---
+++ array test 1 print first letter.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    /*
+        First letter
+        Create a program that lets the user input any string with length 30. Then display the first letter of the string.
+
+        Example:
+        Enter string: hello
+        Output: h
+    */
+    
+    
+    char string[30];
+    
+    cout<<"Enter string:";
+    cin>>string;
+    cout<<string[0];
+    
+    
+    
+    
+    return 0;
+}
